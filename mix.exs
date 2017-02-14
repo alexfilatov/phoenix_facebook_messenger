cd208fef7fcb0483eb3575b27e445e34978564b8
Craig Smith <inicsmith@gmail.com>
2017-02-13 20:30:10 -0600

    pulling in forked deps

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule FacebookMessenger.Phoenix.Mixfile do
   defp deps do
     d =
     [{:phoenix, "~> 1.2"},
-     {:facebook_messenger, "github: inicsmith/facebook_messenger"},
+     {:facebook_messenger, "git: https://github.com/inicsmith/facebook_messenger.git"},
      {:inch_ex, only: :docs},
      {:ex_doc, "~> 0.7", only: :dev},
      {:earmark, "~> 0.1", only: :docs}]
